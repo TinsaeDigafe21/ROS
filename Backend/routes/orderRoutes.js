01db1609fffb069839ae19a2d297281967891af1
nardoki <mihrethabtamu1902@gmail.com>
2026-03-12 00:09:44 +0300

    updating the get order function

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -10,7 +10,7 @@ router.post("/", placeOrder);
 router.get("/", authenticateToken, authorizeRoles("admin", "kitchen"), getAllOrders);
 
 // Protected: get order by ID
-router.get("/:id", authenticateToken, authorizeRoles("admin", "kitchen"), getOrderById);
+router.get("/:id", getOrderById);
 
 // Protected: update order status
 router.put("/:id/status", authenticateToken, authorizeRoles("admin", "kitchen"), updateOrderStatus);
